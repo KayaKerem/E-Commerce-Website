146f4f391aceaecdd4d1535319960c4672d3e9ca
KkutaySarii <h.kutay.1@gmail.com>
2022-03-30 00:05:28 +0300

    pastorders grafiği için gerekli fonksiyon yazıldı

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
+from this import d
 from urllib import request
 from flask import Flask, request
 from initialize_database import initialize
 from flask_cors import CORS
 import database as db
+import json
 app = Flask(__name__)
 CORS(app)
 
@@ -33,7 +35,8 @@ def products():
 
 @app.route("/login", methods = ['GET', 'POST'])
 def login():
-    var = request.form.dict()
+    var = request.data.decode("UTF-8")
+    var = json.loads(var)
     mail = var["mail"]
     password = var["password"]
     if(db.checkUser(mail,password)!=0):
@@ -42,10 +45,31 @@ def login():
         data.update({"id":id})
         data.update({"name":name})
         data.update({"surname":surname})
-        return data
+        return {"data":data}
     else:
-        return 0
+        return {"data":0}
 
+@app.route("/showtable/pastorders")
+def pastorders():
+    id = request.form.dict()
+    temp  = db.totalPackageOfBought(id)#tarih, ürünıd, paket sayısı
+    products = db.getNamesOfProducts()
+    #products =  [{"id":1000, "name":"mukavva"}, {"id":1001,  "name":"hamur"}]
+    categories = []
+    for i in temp:
+        categories.append(i[0])
+    res = []
+    for i in products:
+        datas = []
+        for j in categories:
+            for k in temp:
+                if(i["id"]==k[1] and k[0]==j):
+                    res.append({j["name"]:datas.append(i[2])})
+                else:
+                    res.append({j["name"]:datas.append(0)})
+    return {{"dates": categories},{"pastOrders":res}}             
+                        
+  
 
 app.debug=False
 app.run()
